4f2c5917f921c67d50ddf4606575032e99d873ad
Don Corley <don@donandann.com>
2012-12-12 13:17:30 -0800

    Don't supply filename on mapped record

--- tour/util/src/main/java/com/tourapp/tour/util/test/hotel/db/HotelAvail.java
+++ tour/util/src/main/java/com/tourapp/tour/util/test/hotel/db/HotelAvail.java
@@ -56,13 +56,6 @@ public class HotelAvail extends VirtualRecord
         super.init(screen);
     }
     /**
-     * Get the table name.
-     */
-    public String getTableNames(boolean bAddQuotes)
-    {
-        return (m_tableName == null) ? Record.formatTableNames(HOTEL_AVAIL_FILE, bAddQuotes) : super.getTableNames(bAddQuotes);
-    }
-    /**
      * Get the Database Name.
      */
     public String getDatabaseName()
